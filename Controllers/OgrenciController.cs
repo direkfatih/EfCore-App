403e20afa4e2cfbda52c842406a73c94669d0bb8
[Fatih Hakan Direk] <[fatihhakandirek@gmail.com]>
2024-01-17 21:51:24 +0300

    first commit

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using efCoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace efCoreApp.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ogrenciler = await _context.Ogrenciler.ToListAsync();
+            return View(ogrenciler);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace efCoreApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Kurs> Kurslar { get; set; }
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        public DbSet<KursKayit> KursKayitlari { get; set; }
+
+        // public DbSet<Kurs> Kurslar => Set<Kurs>();
+        // public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+        // public DbSet<KursKayit> KursKayitlari => Set<KursKayit>();
+
+    }
+}
